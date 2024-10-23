de33fa0f3c70a835ecd8019f9f07d3d1b570bead
MichAdebayo <adebayomichael4@gmail.com>
2024-10-23 12:21:58 +0200

    updated main.py in branch develop

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-# This is an addition function
 from addition import addition
-
-print(addition(6, 7))
-
-# This is a division function
 from division import division
 
-print(division(14, 7))
+addition(6, 7)
+division(14, 7)
