84dd65d1ee06d57c8bd89d5fc2ec316a0d1cc70f
Antoine Delvoye <delvoyeadf@gmail.com>
2024-10-23 16:03:23 +0200

    corrected while loop

--- calculatrice.py
+++ calculatrice.py
@@ -8,24 +8,6 @@ from history import history
 
 def calculator():
     l = []
-    while True :
-        print("Welcome to the MAD calculator")
-        print('''
-      _____________________
-     |  _________________  |
-     | |              0. | |
-     | |_________________| |
-     |  ___ ___ ___   ___  |
-     | | 7 | 8 | 9 | | + | |
-     | |___|___|___| |___| |
-     | | 4 | 5 | 6 | | - | |
-     | |___|___|___| |___| |
-     | | 1 | 2 | 3 | | * | |
-     | |___|___|___| |___| |
-     | | . | 0 | = | | / | |
-     | |___|___|___| |___| |
-     |_____________________|
-    ''')
     while True :            
             
         operation = str(input("Enter choice (+, -, *, /, **, ^, sqrt): "))
@@ -150,6 +132,7 @@ def calculator():
         if next_calculation.lower() != 'yes':
             print("Fermeture du supercalculateur MAD")
             return l
+        
         
 
     
