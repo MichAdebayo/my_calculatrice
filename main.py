28bbaa9610dc27003df682b5453f198a2b19a16f
MichAdebayo <adebayomichael4@gmail.com>
2024-10-23 14:00:20 +0200

    added log function to logarithm.py and updated main.py

--- main.py
+++ main.py
@@ -12,10 +12,12 @@ multiplication(4,5)
 exponentiation(3,5)
 sqrt(16)
 from modulo import modulo
+from logarithm import logarithm
 
 print(addition(6, 7))
 print(division(14, 7))
 print(subtraction(5,8))
 print(multiplication(4,5))
 print(modulo(3, 2))
+print(logarithm(20))
 
