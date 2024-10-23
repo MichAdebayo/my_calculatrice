3107181094240cf5c6985ede861068d896bf1610
David <david.scott.2875@gmail.com~>
2024-10-23 14:30:27 +0200

    updated main

--- main.py
+++ main.py
@@ -7,22 +7,10 @@ from exponentiation import exponentiation
 from sqrt import sqrt
 from modulo import modulo
 from logarithm import logarithm
-
-print(addition(6, 7))
-print(division(14, 7))
-print(subtraction(5,8))
-print(multiplication(4,5))
-print(modulo(3, 2))
-print(logarithm(20))
 from trigonometry import sin,cos,tan
 from unit_conversion import *
 
-addition(6, 7)
-division(14, 7)
-subtraction(5,8)
-multiplication(4,5)
-exponentiation(3,5)
-sqrt(16)
+
 sin(45)
 cos(45)
 tan(45)
