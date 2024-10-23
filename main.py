047c27b7e264399181c4c643c270d1e20e7ea1ec
David <david.scott.2875@gmail.com~>
2024-10-23 13:45:51 +0200

    added sqrt and exponential to main

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ from addition import addition
 from division import division
 from subtraction import subtraction
 from multiplication import multiplication
+from exponentiation import exponentiation
+from sqrt import sqrt
 
 addition(6, 7)
 division(14, 7)
 subtraction(5,8)
 multiplication(4,5)
+exponentiation(3,5)
+sqrt(16)
 
