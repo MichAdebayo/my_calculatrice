d2938f0a9b6d73b571ef1eb546b4f37c349bc239
Antoine Delvoye <delvoyeadf@gmail.com>
2024-10-23 14:13:32 +0200

    add ** and sqrt to calculatrice

--- calculatrice.py
+++ calculatrice.py
@@ -9,19 +9,25 @@ def calculator():
     n1 = float(input("Enter first number: "))
     n2 = float(input("Enter second number: "))
 
-    operation = str(input("Enter choice (+, -, *, /): "))
+    operation = str(input("Enter choice (+, -, *, /, **, ^, sqrt): "))
 
     if operation == "+":
         return print(f"{n1} + {n2} = {addition(n1,n2)}")
     
     if operation == "-":
-        return print(f"{n1} - {n2} = {substraction(n1,n2)}")
+        return print(f"{n1} - {n2} = {subtraction(n1,n2)}")
 
     if operation == "/":
         return print(f"{n1} / {n2} = {division(n1,n2)}")
 
     if operation == "*":
         return print(f"{n1} * {n2} = {multiplication(n1,n2)}")   
+    
+    if operation == "**" or  operation == '^':
+        return print(f"{n1} * {n2} = {exponentiation(n1,n2)}")  
+    
+    if operation == "sqrt":
+        return print(f"sqrt {n1} = {sqrt(n1)}")  
     
 
 
