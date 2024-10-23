f4abc038196f1e5db8b08ee8849378b4ebc1a714
David <david.scott.2875@gmail.com~>
2024-10-23 14:07:26 +0200

    add trig functions to main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from subtraction import subtraction
 from multiplication import multiplication
 from exponentiation import exponentiation
 from sqrt import sqrt
+from trigonometry import sin,cos,tan
 
 addition(6, 7)
 division(14, 7)
@@ -11,4 +12,7 @@ subtraction(5,8)
 multiplication(4,5)
 exponentiation(3,5)
 sqrt(16)
+sin(45)
+cos(45)
+tan(45)
 
