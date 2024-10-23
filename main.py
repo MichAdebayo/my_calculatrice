d00b4ab1c2ea95adb959b0f5f7e39e66ded1001f
MichAdebayo <adebayomichael4@gmail.com>
2024-10-23 12:30:36 +0200

    updated main.py with other functions

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
-<<<<<<< HEAD
 from addition import addition
 from division import division
-
-addition(6, 7)
-division(14, 7)
-=======
 from subtraction import subtraction
 from multiplication import multiplication
 
+addition(6, 7)
+division(14, 7)
 subtraction(5,8)
-n_multiply = multiplication(4,5)
+multiplication(4,5)
 
->>>>>>> origin/develop
