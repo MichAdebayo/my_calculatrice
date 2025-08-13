acd8a82501aea210a14ae843adf7319626a55dbf
MichAdebayo <adebayomichael4@gmail.com>
2025-08-13 15:25:29 +0200

    refactor: added docstring for square root function

--- sqrt.py
+++ sqrt.py
@@ -1,2 +1,12 @@
 def sqrt(x):
-    return x ** .5
+    """Calculates the square root of a number.
+
+    This function returns the square root of the given numeric value.
+
+    Args:
+        x: The number for which to calculate the square root.
+
+    Returns:
+        The square root of x.
+    """
+    return x ** 0.5
