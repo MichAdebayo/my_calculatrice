96f8c9f6df73b0b5821dbc7d97fbb9a82d7a3fd9
MichAdebayo <adebayomichael4@gmail.com>
2025-08-13 15:27:43 +0200

    refactor: added docstrings for unit conversion functions

--- unit_conversion.py
+++ unit_conversion.py
@@ -1,11 +1,51 @@
 def kilo_to_miles(x):
+    """Converts kilometers to miles.
+
+    This function takes a distance in kilometers and returns the equivalent distance in miles.
+
+    Args:
+        x: The distance in kilometers.
+
+    Returns:
+        The distance converted to miles.
+    """
     return x * 0.621371
 
 def miles_to_kilo(x):
-    return x * 1.09344
+    """Converts miles to kilometers.
+
+    This function takes a distance in miles and returns the equivalent distance in kilometers.
+
+    Args:
+        x: The distance in miles.
+
+    Returns:
+        The distance converted to kilometers.
+    """
+    return x * 1.60934
 
 def kg_to_lb(x):
-    return x*2.2
+    """Converts kilograms to pounds.
+
+    This function takes a weight in kilograms and returns the equivalent weight in pounds.
+
+    Args:
+        x: The weight in kilograms.
+
+    Returns:
+        The weight converted to pounds.
+    """
+    return x * 2.2
 
 def lb_to_kg(x):
-    return x/2.2
+    """Converts pounds to kilograms.
+
+    This function takes a weight in pounds and returns the equivalent weight in kilograms.
+
+    Args:
+        x: The weight in pounds.
+
+    Returns:
+        The weight converted to kilograms.
+    """
+    return x / 2.2
