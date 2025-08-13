d61cbfe820dda6a2a301350a94bab603ca01a459
MichAdebayo <adebayomichael4@gmail.com>
2025-08-13 15:34:51 +0200

    refactor: improved docstring for calculator function and enhanced input handling

--- calculatrice.py
+++ calculatrice.py
@@ -7,24 +7,32 @@ from sqrt import sqrt
 from history import history
 
 def calculator():
+    """
+    Runs an interactive calculator for basic arithmetic operations.
+    This function allows users to perform addition, subtraction, multiplication,
+    division, exponentiation, and square root calculations interactively, and logs 
+    the calculation history.
 
-    l = []
+    """
+
+    l = [] # List to store calculation history
     operations = ["+", "-" , "*" , "/" , "**" , "^" , "sqrt"]
 
+    # Prompt user for a valid operation
     while True:
-        
+
         try:            
-            
+
             operation = str(input("Enter choice (+, -, *, /, **, ^, sqrt): "))
 
             if operation in operations:
                 break
             else:
                 raise ValueError
-        
+
         except ValueError:
                 print("Enter a valid operation")
-    
+
     while True:
 
         if operation == 'sqrt':
@@ -46,13 +54,14 @@ def calculator():
             | |___|___|___| |___| |
             |_____________________|
             ''')
-        
+
             l.append(history("sqrt", n1, result=result))
         else:
             n1 = float(input("Enter first number: "))
             n2 = float(input("Enter second number: "))
             result = None
-        
+
+            # Perform the selected operation
             if operation == "+":
                 result = addition(n1,n2)
 
@@ -65,13 +74,14 @@ def calculator():
 
             elif operation == "*":
                 result = multiplication(n1,n2)
-            
-            elif operation == "**" or  operation == '^':
+
+            elif operation in ["**", '^']:
                 result = exponentiation(n1,n2)
-            
+
             else:
                 print("Error: enter correct operation")
 
+            # Display result and add to history if calculation was successful
             if result is not None:
                 print(f'''
                 _____________________
@@ -92,22 +102,13 @@ def calculator():
 
                 l.append(history(operation, n1, n2, result))
 
-
         next_calculation = str(input("Do you want to perform another calculation? (yes/no): "))
 
         if next_calculation.lower() != 'yes':
             print("Fermeture du supercalculateur MAD")
-            
-            # Write to log file
+
+            # Write calculation history to log file
             with open("log_calculations.txt", "w") as f:
                 for entry in l:
                     f.write(entry + "\n")  # Ensure each entry is a string
             break
-        
-
-
-
-        
-        
-
-    
