7391a1ccc1552852d7b12aded0dc310b1c6238be
MichAdebayo <adebayomichael4@gmail.com>
2025-08-13 15:26:10 +0200

    refactor: added docstring for subtraction function

--- subtraction.py
+++ subtraction.py
@@ -1,2 +1,13 @@
-def subtraction(x,y):
-    return x-y
+def subtraction(x, y):
+    """Returns the difference between two numbers.
+
+    This function subtracts the second numeric value from the first and returns the result.
+
+    Args:
+        x: The number from which to subtract.
+        y: The number to subtract.
+
+    Returns:
+        The result of x minus y.
+    """
+    return x - y
