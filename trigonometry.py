503e6acf63aaaa1ecd0437e5a2387c0f639fc66a
MichAdebayo <adebayomichael4@gmail.com>
2025-08-13 15:26:57 +0200

    refactor: added docstrings for trigonometric functions

--- trigonometry.py
+++ trigonometry.py
@@ -1,10 +1,40 @@
 import math
 
 def sin(x):
+    """Calculates the sine of a given angle.
+
+    This function returns the sine of the input value x, interpreted as radians.
+
+    Args:
+        x: The angle in radians.
+
+    Returns:
+        The sine of x.
+    """
     return math.sin(x)
 
 def cos(x):
+    """Calculates the cosine of a given angle.
+
+    This function returns the cosine of the input value x, interpreted as radians.
+
+    Args:
+        x: The angle in radians.
+
+    Returns:
+        The cosine of x.
+    """
     return math.cos(x)
 
 def tan(x):
+    """Calculates the tangent of a given angle.
+
+    This function returns the tangent of the input value x, interpreted as radians.
+
+    Args:
+        x: The angle in radians.
+
+    Returns:
+        The tangent of x.
+    """
     return math.tan(x)
